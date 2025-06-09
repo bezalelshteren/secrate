3f51dc580035ec69ea9794f44bbad4fc1357f218
bezalelshteren <a0556722346@gmail.com>
2025-06-09 14:59:35 +0300

    Add project files.

---
+++ getNames.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secrate
+{
+    public class DHLsecrate
+    {
+        public void GetPersonByName()//לפי השם
+        {
+            try
+            {
+                Console.WriteLine("Enter your name and the name of the target and report separated by a semicolon");
+                string[] allInfo = Console.ReadLine().Split();
+                var nameOrId = allInfo[0];
+                string report = allInfo[1];
+                
+            }
+
+        }
+        public void GetPersonBySecretCode() //
+        {
+
+        }
+        public void InsertNewPerson() { }//
+        public void InsertIntelReport() { }//
+        public void UpdateReportCount() { }//
+        public void UpdateMentionCount() { }//
+        public void GetReporterStats() { }//
+        public void GetTargetStats() { }//
+        public void CreateAlert() { }//
+        public void GetAlerts() { }//
+    }
+}
